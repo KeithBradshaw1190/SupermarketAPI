b03ee0ca26b1caba6a8d8346c6ff13f5031e91eb
Keith B <keithbradshaw0@gmail.com>
2020-04-07 23:51:15 +0100

    wip

--- index.js
+++ index.js
@@ -23,24 +23,10 @@ const pickup = require("./routes/pickup");
 //const cors = require('cors')
 
 //app.use(cors());
-app.use(allowCrossDomain)
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect 'https://smartgrocery-manager.herokuapp.com' 
-
-  res.setHeader('Access-Control-Allow-Origin', 'smartgrocery-manager.herokuapp.com/');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 // Configure bodyparser to handle post requests
